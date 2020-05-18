8ae65cac0800d11673c129c0c6b865aa1a537f7b
Amit Kulkarni <43036934+amitkulkarni1905@users.noreply.github.com>
2020-05-19 05:03:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,112 +1,48 @@
 import dash
+from dash.dependencies import Output, Event
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly
 import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+import collections
 
-########### Define your variables
-elements=['High carbon steel', 'Low carbon steel', 'Cast iron', 'Aluminium alloys', 'Stainless steel','Carbon fibre composite','Nickle alloy','Copper alloys']
-density=[7.5,7.6,7.2,2.54,7.8,1.4,8.42,8.64]
-p_size=[9.27, 0,0,51,18.1,0,39,18.3]
-h_brinell = [260,247,217,97.1,251,63.3,282,146]
-c_impact = [34.1,54.9,7.91,115,151,47.8]
+app = dash.Dash()
 
-tabtitle='Mech Project'
-myheading='Mech Project'
-label1='Density'
-label2='Particle Size'
-
-########### Set up the chart
-density1 = go.Bar(
-    x=elements,
-    y=density,
-    name=label1,
-    marker={'color':'red'}
-)
-g_data = [density1]
-g_layout = go.Layout(
-    barmode='group',
-    title = 'Density'
-)
-
-g_fig = go.Figure(data=g_data, layout=g_layout)
-
-"""....................................................................................."""
-
-p_size1 = go.Bar(
-    x=elements,
-    y=p_size,
-    name=label2,
-    marker={'color':'darkgreen'}
-)
-g_d2 = [p_size1]
-g_layout2 = go.Layout(
-    barmode='group',
-    title = 'Particle Size'
-)
-
-g_fig2 = go.Figure(data=g_d2, layout=g_layout2)
-
-"""....................................................................................."""
-
-"""....................................................................................."""
-c_impact = go.Bar(
-    x  =  elements,
-    y = c_impact,
-    marker = {'color' : 'red'}
-)
-g_d4 = [c_impact]
-g_layout4 = go.Layout(
-    barmode = 'group',
-    title = 'c_imapct'
-)
-
-g_fig4 = go.Figure(data=g_d4,layout=g_layout4)
-
-h_brinell = go.Bar(
-       x = elements,
-       y = h_brinell,
-       marker = {'color' : 'blueviolet'}
-)
-g_d3 = [h_brinell]
-g_layout3 = go.Layout(
-    barmode = 'group',
-    title = 'Hardness (Brinell)'
-)
-
-g_fig3 = go.Figure(data=g_d3,layout=g_layout3)
-
-
-
-########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
-app.title=tabtitle
-
-########### Set up the layout
 app.layout = html.Div(children=[
-    dcc.Graph(
-        id='f1',
-        figure=g_fig
+    html.H1(children='''
+        Interactive Chart:
+    '''),
+    dcc.Dropdown(
+        id = "input",
+        options=[
+            {'label': 'January', 'value': 1},
+            {'label': 'Febuary', 'value': 2},
+            {'label': 'March', 'value': 3}
+        ], value = 1
     ),
-    dcc.Graph(
-        id='f2',
-        figure=g_fig2
-    ),
-  
-    dcc.Graph(
-        id = 'f4',
-        figure = g_fig4
-    )
-    
-  dcc.Graph(
-        id = 'f3',
-        figure = g_fig3
-    )
-  
-   
-    ]
-)
+    dcc.Graph( id="output-graph")
+])
 
-if __name__ == '__main__':
-    app.run_server()
+@app.callback(
+    Output(component_id='output-graph', component_property='figure'),
+    [Input(component_id='input', component_property='value')])
+def update_value(value):
+
+    start = datetime.datetime(2018, value, 1, 0, 0, 0, 1)
+    end = datetime.datetime(2018,  value + 1, 1, 0, 0, 0, 1)
+    subset_df = df[ (df["lost_time"] > start) & (df["lost_time"] < end) ]
+    x = pd.value_counts(subset_df.deal_source).index
+    y = pd.value_counts(subset_df.deal_source).values
+
+    return({'data': [
+                {'x': x, 'y': y, 'type': 'bar', 'name': value},
+            ],
+            'layout': {
+                'title': "Deal Flow source for {} 2018".format(months[value-1])
+            }
+        }
+    )
+
+if __name__ == "__main__":
+    app.run_server(debug = True)
