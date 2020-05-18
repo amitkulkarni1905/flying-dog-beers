46a1692665c4be8d65a3529ecad05827ac122c2f
Amit Kulkarni <43036934+amitkulkarni1905@users.noreply.github.com>
2020-05-18 18:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objs as go
 elements=['High carbon steel', 'Low carbon steel', 'Cast iron', 'Aluminium alloys', 'Stainless steel','Carbon fibre composite','Nickle alloy','Copper alloys']
 density=[7.5,7.6,7.2,2.54,7.8,1.4,8.42,8.64]
 p_size=[9.27, 0,0,51,18.1,0,39,18.3]
-h_brinell = [260,247,217,97.1,251,63.3,282,146]
+
 
 tabtitle='Mech Project'
 myheading='Mech Project'
@@ -47,19 +47,7 @@ g_fig2 = go.Figure(data=g_d, layout=g_layout2)
 
 """....................................................................................."""
 
-h_brinell = go.Bar(
-    x=elements,
-    y=h_brinell,
-    name=label3,
-    marker={'color':'darkblue'}
-)
-g_d3 = [h_brinell]
-g_layout3 = go.Layout(
-    barmode='group',
-    title = 'Hardness(Brinell)'
-)
 
-g_fig3 = go.Figure(data=g_d3, layout=g_layout3)
 
 
 
@@ -80,10 +68,7 @@ app.layout = html.Div(children=[
         id='f2',
         figure=g_fig2
     ),
-     dcc.Graph(
-        id='f3',
-        figure=g_fig3
-    ),
+   
 
    
     ]
