65bc7ea55eb8edc841ed1155e0415192caf1015c
Amit Kulkarni <43036934+amitkulkarni1905@users.noreply.github.com>
2020-05-19 05:18:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,63 @@
 import dash
-from dash.dependencies import Output, Event
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly
 import plotly.graph_objs as go
-from dash.dependencies import Input, Output
-import collections
 
-app = dash.Dash()
+########### Define your variables
+beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+ibu_values=[35, 60, 85, 75]
+abv_values=[5.4, 7.1, 9.2, 4.3]
+color1='lightblue'
+color2='darkgreen'
+mytitle='Beer Comparison'
+tabtitle='beer!'
+myheading='Flying Dog Beers'
+label1='IBU'
+label2='ABV'
+githublink='https://github.com/austinlasseter/flying-dog-beers'
+sourceurl='https://www.flyingdog.com/beers/'
 
+########### Set up the chart
+bitterness = go.Bar(
+    x=beers,
+    y=ibu_values,
+    name=label1,
+    marker={'color':color1}
+)
+alcohol = go.Bar(
+    x=beers,
+    y=abv_values,
+    name=label2,
+    marker={'color':color2}
+)
+
+beer_data = [bitterness, alcohol]
+beer_layout = go.Layout(
+    barmode='group',
+    title = mytitle
+)
+
+beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+
+
+########### Initiate the app
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+app.title=tabtitle
+
+########### Set up the layout
 app.layout = html.Div(children=[
-    html.H1(children='''
-        Interactive Chart:
-    '''),
-    dcc.Dropdown(
-        id = "input",
-        options=[
-            {'label': 'January', 'value': 1},
-            {'label': 'Febuary', 'value': 2},
-            {'label': 'March', 'value': 3}
-        ], value = 1
+    html.H1(myheading),
+    dcc.Graph(
+        id='flyingdog',
+        figure=beer_fig
     ),
-    dcc.Graph( id="output-graph")
-])
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A('Data Source', href=sourceurl),
+    ]
+)
 
-@app.callback(
-    Output(component_id='output-graph', component_property='figure'),
-    [Input(component_id='input', component_property='value')])
-def update_value(value):
-
-    start = datetime.datetime(2018, value, 1, 0, 0, 0, 1)
-    end = datetime.datetime(2018,  value + 1, 1, 0, 0, 0, 1)
-    subset_df = df[ (df["lost_time"] > start) & (df["lost_time"] < end) ]
-    x = pd.value_counts(subset_df.deal_source).index
-    y = pd.value_counts(subset_df.deal_source).values
-
-    return({'data': [
-                {'x': x, 'y': y, 'type': 'bar', 'name': value},
-            ],
-            'layout': {
-                'title': "Deal Flow source for {} 2018".format(months[value-1])
-            }
-        }
-    )
-
-if __name__ == "__main__":
-    app.run_server(debug = True)
+if __name__ == '__main__':
+    app.run_server()
