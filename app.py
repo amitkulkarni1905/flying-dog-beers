b8f5b133582c72db60a4dd6dd926e664c998ad47
Amit Kulkarni <43036934+amitkulkarni1905@users.noreply.github.com>
2020-05-19 04:21:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ g_layout2 = go.Layout(
 g_fig2 = go.Figure(data=g_d2, layout=g_layout2)
 
 """....................................................................................."""
-h_brinell = go.Bar(
+"""h_brinell = go.Bar(
        x = elements,
        y = h_brinell,
        marker = {'color' : 'blueviolet'}
@@ -58,7 +58,7 @@ g_layout3 = go.Layout(
     title = 'Hardness (Brinell)'
 )
 
-g_fig3 = go.Figure(data=g_d3,layout=g_layout3)
+g_fig3 = go.Figure(data=g_d3,layout=g_layout3)"""
 
 """....................................................................................."""
 h_rockb = go.Bar(
@@ -69,7 +69,7 @@ h_rockb = go.Bar(
 g_d4 = [h_rockb]
 g_layout4 = go.Layout(
     barmode = 'group',
-    title = 'Hardness (Brinell)'
+    title = 'Hardness Rockwell B'
 )
 
 g_fig4 = go.Figure(data=g_d4,layout=g_layout4)
@@ -92,10 +92,10 @@ app.layout = html.Div(children=[
         id='f2',
         figure=g_fig2
     ),
-    dcc.Graph(
+    """dcc.Graph(
         id = 'f3',
         figure = g_fig3
-    )
+    )"""
    dcc.Graph(
         id = 'f4',
         figure = g_fig4
