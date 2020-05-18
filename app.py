b86b3ec019f8432d40ae4918db81635758e3b7b0
Amit Kulkarni <43036934+amitkulkarni1905@users.noreply.github.com>
2020-05-18 05:39:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,7 @@ elements=['High carbon steel', 'Low carbon steel', 'Cast iron', 'Aluminium alloy
 density=[7.5,7.6,7.2,2.54,7.8,1.4,8.42,8.64]
 p_size=[9.27, 0,0,51,18.1,0,39,18.3]
 h_brinell = [260,247,217,97.1,251,63.3,282,146]
-h_rockb = [75.7,	89.4,	0,	69.3,	54,	88.3,	88.2,	69.2]
-h_rockc = [46.4,	31.7,	17.9,	45.2,	86.4,	0,	38,	34.7]
-v_hard = [286,	266,	235,	116,	294,	1030,	296,	231]
-t_ultima = [1120,	716,	310,	344,	941,	1230,	902,	512]
-t_yield = [896,	519,	124,	278,	669,	1.55,	585,	384]
-e_break = [14.30,	20.20,	6.67,	9.94,	26.50,	94.60,	33.80,	22.60]
-mod_e = [200,	202,	118,	77.4,	196,	0,	207,   119]
-s_mod = [79.9,	79.7,	47.6,	19.9,	77.6,	3.11,	77.5,	44.9]
-b_mod = [160,	160,	0,	0,	166,	0,	0,	140]
-p_ratio = [0.292,	0.29,	0.294,	0.327,	0.289,	0.293,	0.316,	0.317]
-fy_strength = [2260,	0,	575,	260,	0,	999,	689,	525]
-cy_strength = [2160,	0,	943,	95.7,	0,	725,	0,	529]
-m = [53.05,	61.30,	48.80,	55.90,	31.70,	0,	0,	32.60]
-i_impact = [8.9,	96.5,	29.7,	0,	115,	10.4,	131,	33.7]
-c_impact = [34.1,	54.9,	0,	7.91,	115,	0,	151,	47.8]
+
 tabtitle='Mech Project'
 myheading='Mech Project'
 label1='Density'
@@ -63,7 +49,7 @@ g_fig2 = go.Figure(data=g_d, layout=g_layout2)
 
 h_brinell = go.Bar(
     x=elements,
-    y=p_size,
+    y=h_brinell,
     name=label2,
     marker={'color':'blue'}
 )
@@ -95,7 +81,7 @@ app.layout = html.Div(children=[
         figure=g_fig2
     ),
      dcc.Graph(
-        id='f2',
+        id='f3',
         figure=g_fig3
     ),
     
